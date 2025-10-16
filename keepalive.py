a8aa68552af383416dff718aab48f9115825243a
isaias4321 <151641175+isaias4321@users.noreply.github.com>
2025-10-16 16:14:27 -0300

    Update keepalive.py

--- keepalive.py
+++ keepalive.py
@@ -1,1 +1,15 @@
-# keepalive script
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Amazon Affiliate ativo!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
